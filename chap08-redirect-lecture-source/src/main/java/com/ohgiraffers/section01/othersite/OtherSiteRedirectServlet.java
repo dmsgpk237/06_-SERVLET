b311e69aa1e3f52b454da11e86caea561912f85c
dmsgpk237 <a01027947353@gmail.com>
2024-07-02 16:44:06 +0900

    chap07

---
+++ chap08-redirect-lecture-source/src/main/java/com/ohgiraffers/section01/othersite/OtherSiteRedirectServlet.java
@@ -0,0 +1,18 @@
+package com.ohgiraffers;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.WebServlet;
+
+import java.io.IOException;
+
+@WebServlet("/othersite")
+public class OtherSiteRedirectServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("get 요청 후 naver 사이트로 redirect");
+        response.sendRedirect("https://www.naver.com/");
+    }
+
+
+}
